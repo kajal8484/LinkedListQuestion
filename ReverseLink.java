4fa954a1e586449287582b8f2f0b6f56733db091
kajal8484 <47441622+kajal8484@users.noreply.github.com>
2026-01-18 23:36:57 +0530

    Implement reverse linked list in Solution class
    Refactored the code to implement a reverse linked list function using a new class structure.

--- ReverseLink.java
+++ ReverseLink.java
@@ -1,47 +1,26 @@
-package LinkedListQuestion;
-
-import java.util.Scanner;
-
-public class ReverseLink {
-
-	    public static  Node insert(Node head,int data) {
-	        //Complete this method
-	        Node node=new Node(data);
-	        Node n=head;
-	        if(head==null)
-	        {
-	            head=node;
-	        }
-	        else{
-	        while(n.next!=null)
-	        {
-	            n=n.next;
-	        }
-	        n.next=node;
-	        }
-	         
-	        return head;
-	        
-	    }
-
-		public static void display(Node head) {
-	        Node start = head;
-	        while(start != null) {
-	            System.out.print(start.data + " ");
-	            start = start.next;
-	        }
-	    }
-
-	    public static void main(String args[]) {
-	        Scanner sc = new Scanner(System.in);
-	        Node head = null;
-	        int N = sc.nextInt();
-
-	        while(N-- > 0) {
-	            int ele = sc.nextInt();
-	            head = insert(head,ele);
-	        }
-	        display(head);
-	        sc.close();
-	    }
-	}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+      
+        ListNode prev=null;
+        while(curr!=null)
+        {  ListNode next=curr.next;
+           curr.next=prev;
+prev=curr;
+curr=next;
+            
+           
+        }
+        return prev;
+    }
+}
